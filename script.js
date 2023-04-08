cf97283124e3fbc82ca551a2ddbc89c637992570
Anas <m2005boss@gmail.com>
2023-04-08 06:55:55 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// var typed = new Typed(".multiple-text", {
+//     Strings: ["Frontend ", "Backend", "Fullstack"],
+//     typespeed: 100,
+//     backspeed: 100,
+//     backDelay: 1000,
+//     loop: true
+// })
+
+var typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer ", "Backend Developer", "Fullstack Developer", "Youtuber", "student of 1st year", "student of Metaverse at Panaverse"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
+
+
+// s = { strings: ["These are the default values...", "You know what you should do?", "Use your own!", "Have a great day!"] }
